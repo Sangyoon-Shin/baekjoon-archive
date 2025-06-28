483793778db11d950123ca75b3bd4043ada5856c
sangyoon_2.23 <tlstkddbs223@naver.com>
2025-06-28 17:26:01 +0900

    [BOJ 11653] 소인수분해 solved

---
+++ 09_Factors_Multiples_Primes_1/11653.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def solution(n):
+    flag = 0
+    while True:
+        if flag == 1:
+            break
+        for i in range(2, n + 1):
+            if n % i == 0 and n // i != 1:
+                n = n // i
+                print(i)
+                break
+            elif n % i == 0 and n // i == 1:
+                flag = 1
+                print(i)
+                break
+    
+n = int(input())
+solution(n)
