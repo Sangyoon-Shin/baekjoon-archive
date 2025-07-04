faadee76e88755b3e510b6393987309f1ac319ce
sangyoon_2.23 <tlstkddbs223@naver.com>
2025-07-04 17:41:11 +0900

    [BOJ 11653] 소인수 분해

--- 09_Factors_Multiples_Primes_1/11653.py
+++ 09_Factors_Multiples_Primes_1/11653.py
@@ -3,18 +3,21 @@ input = sys.stdin.readline
 
 def solution(n):
     flag = 0
-    while True:
-        if flag == 1:
-            break
-        for i in range(2, n + 1):
-            if n % i == 0 and n // i != 1:
-                n = n // i
-                print(i)
+    if n == 1:
+        pass
+    else:
+        while True:
+            if flag == 1:
                 break
-            elif n % i == 0 and n // i == 1:
-                flag = 1
-                print(i)
-                break
+            for i in range(2, n + 1):
+                if n % i == 0 and n // i != 1:
+                    n = n // i
+                    print(i)
+                    break
+                elif n % i == 0 and n // i == 1:
+                    flag = 1
+                    print(i)
+                    break
     
 n = int(input())
 solution(n)
