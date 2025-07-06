492c9f94140dd1a34e3b4ac8e4578aa47f6b3aac
sangyoon_2.23 <tlstkddbs223@naver.com>
2025-07-06 20:35:43 +0900

    [BOJ 1065] 한수

--- python_practice/실버4/1065.py
+++ python_practice/실버4/1065.py
@@ -1,13 +1,30 @@
-# import sys
-# input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-# n = int(input())
-# arr = []
+res = [True for i in range(1001)]
 
-# for i in range(n):
-#     arr = list(str(i))
-#     if len(arr) < 10:
-#         print(len(arr))
-#     else:
-#         for j in range(len(arr) - 1):
-#             if arr[j + 1] - arr[j] == 
+for i in range(1, 1001):
+    i = str(i)
+    stringlist = list(i)
+    flag = 0
+    if len(stringlist) >= 3:
+        for j in range(len(stringlist) - 1):
+            if j == 0:
+                gap = int(stringlist[j + 1]) - int(stringlist[j])
+            else:
+                curgap = int(stringlist[j + 1]) - int(stringlist[j])
+                if curgap != gap:
+                    flag = 1
+                    value = int(''.join(stringlist))
+                    res[value] = False
+    else:
+        pass
+
+n = int(input())
+
+cnt = 0
+for i in range(1, n + 1):
+    if res[i] == True:
+        cnt += 1
+
+print(cnt)
