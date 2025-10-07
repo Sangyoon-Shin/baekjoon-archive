f89139e6b854518142ea0b1354b7846abcae9eca
sangyoon_2.23 <tlstkddbs223@naver.com>
2025-10-07 16:33:24 +0900

    [BOJ 1402] 아무래도이문제A번난이도인것같다

--- ad_hoc/1402.py
+++ ad_hoc/1402.py
@@ -2,11 +2,9 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-a, b = map(int, input().split())
-
-if a == 0 or b == 0:
-    print("no")
-elif a == 1 and b == 1:
-    print("no")
-else:
+for i in range(n):
+    a, b = map(int, input().split())
     print("yes")
+
+# a < b: a * 1 * 1... 이런식으로 해버리면 a + 1 + 1 로 b를 만들어낼 수 있고
+# a > b: a * (-1) * (-1) * 1... 이렇게 하면 a - 1 - 1 + 1 이니까 a - 1, 즉 a보다 작은 값도 만들어 낼 수 있다.
