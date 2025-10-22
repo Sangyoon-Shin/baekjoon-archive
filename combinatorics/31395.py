f3088cd2789e05ac36dbd0de1d2fd14f84fe8546
sangyoon_2.23 <tlstkddbs223@naver.com>
2025-10-22 16:00:38 +0900

    [BOJ 31395] 정렬된 연속한 부분수열의 개수

--- combinatorics/31395.py
+++ combinatorics/31395.py
@@ -4,6 +4,9 @@ input = sys.stdin.readline
 n = int(input())
 seq = list(map(int, input().split()))
 
+# 0번 idx부터 오름차순으로 이루어진 구간의 길이를 먼저 세고
+# 만약에 오름차순이 끊기는 구간이 생기면 그때까지의 길이를 기반으로 cnt * (cnt + 1) // 2를 해준다.
+# i번째부터 j번째까지 고를 수 있는 조합의 수가 cnt * (cnt + 1) // 2
 cnt = 1
 total = 0
 
