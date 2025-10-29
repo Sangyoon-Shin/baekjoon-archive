0ed1728454e54fd014b6489deb1820859102f514
sangyoon_2.23 <tlstkddbs223@naver.com>
2025-10-30 00:59:55 +0900

    [BOJ 2217] 로프

--- greedy/2217.py
+++ greedy/2217.py
@@ -9,6 +9,7 @@ for i in range(n):
 weight.sort(reverse=True)
 maximum = weight[0]
 
+# 내림차순 정렬을 한 거기 때문에, weight[i] 이전까지의 로프들은 무조건 weight[i]만큼 들 수 있음. 
 for i in range(1, n):
     if maximum < weight[i] * (i + 1):
         maximum = weight[i] * (i + 1)
